078c04449e37923ec2eeeed8a68decf6fc2a30e3
HJK <ttlulu408@gmail.com>
2026-05-21 23:13:25 +0700

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc, doc, updateDoc, deleteDoc, onSnapshot, query, orderBy, serverTimestamp, increment, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+const cfg = {
+  apiKey: "AIzaSyDeL2zJ2c9hr8xMGmzbclSaq_B2dkwVAbA",
+  authDomain: "kabarnusantara-68abc.firebaseapp.com",
+  projectId: "kabarnusantara-68abc",
+  storageBucket: "kabarnusantara-68abc.firebasestorage.app",
+  messagingSenderId: "411274357104",
+  appId: "1:411274357104:web:3c045d04f3e941483147b5"
+};
+
+const app = initializeApp(cfg);
+const db = getFirestore(app);
+enableIndexedDbPersistence(db).catch(() => {});
+
+window.KN_FB = { db, collection, addDoc, doc, updateDoc, deleteDoc, onSnapshot, query, orderBy, serverTimestamp, increment };
+window.dispatchEvent(new Event("fb_ready"));
